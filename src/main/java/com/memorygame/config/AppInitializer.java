e556955900d779ccdd8090e39843c15aefc31f49
Meriem-ui4 <179345308+Meriem-ui4@users.noreply.github.com>
2026-04-21 13:46:34 +0100

    update project

---
+++ src/main/java/com/memorygame/config/AppInitializer.java
@@ -0,0 +1,28 @@
+package com.memorygame.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Point d'entree de l'application web.
+ * Remplace le fichier web.xml en utilisant l'API Servlet 3.0.
+ * Spring MVC detecte automatiquement cette classe au demarrage de Tomcat.
+ *
+ * Le DispatcherServlet est mappe sur "/" pour intercepter toutes les requetes.
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{ "/" };
+    }
+}
